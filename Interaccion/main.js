6ae46538cc7dabd6932702ae8200e3f584181fba
Fabiana <taruka37@gmail.com>
2023-09-14 19:24:57 -0300

    corrección de bacground-clip

---
+++ Interaccion/main.js
@@ -0,0 +1,49 @@
+"use strict"
+// CAMBIO DE CARD
+toggle.onclick = () =>{
+    items.classList.toggle("open")
+    toggle.classList.toggle("close")
+}
+
+
+// SCROLL
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el)
+    }
+  })
+}
+
+window.addEventListener("scroll", () => {
+  handleScrollAnimation();
+});
